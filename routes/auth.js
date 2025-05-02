4c4e7147a7105293b0a1fd81e5174f989c3d7286
arlaspresident <jankovic4901@gmail.com>
2025-05-02 11:16:39 +0200

    la till inloggning med lösenordsverifiering och token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const db = require('../db/database');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -31,5 +32,44 @@ router.post('/register', (req, res) => {
     res.status(201).json({ message: 'användare registrerad', userId: this.lastID });
   });
 });
+
+// post /api/login
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: 'fyll i både användarnamn och lösenord.' });
+    }
+  
+    const sql = `SELECT * FROM users WHERE username = ?`;
+  
+    db.get(sql, [username], (err, user) => {
+      if (err) {
+        return res.status(500).json({ message: 'serverfel' });
+      }
+  
+      if (!user) {
+        return res.status(401).json({ message: 'felaktigt användarnamn eller lösenord' });
+      }
+  
+      const passwordMatch = bcrypt.compareSync(password, user.password);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({ message: 'felaktigt användarnamn eller lösenord' });
+      }
+  
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      );
+  
+      return res.status(200).json({
+        message: 'inloggning lyckades!',
+        token: token
+      });
+    });
+  });
+  
 
 module.exports = router;
